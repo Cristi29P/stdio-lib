a8a8e4de7a49e155c5832809a7983c829224cc61
Cristian Paris <cristian.tanasep@gmail.com>
2022-03-31 20:42:55 +0300

    Added fread

--- checker-lin/so_stdio.c
+++ checker-lin/so_stdio.c
@@ -3,12 +3,12 @@
 //
 #include "so_stdio.h"
 #include "useful.h"
+#include <stdio.h>
 
 struct _so_file {
 	int fd; /* The associated file descriptor */
 	int file_pointer; /* Cursor position inside file. */
 	int buffer_pointer; /* Cursor position inside buffer */
-	//int bytes_read_last_time;
 	char buffer[BUFF_SIZE]; /* The associated file buffer used for I/O tasks */
 	char file_name[PATH_LENGTH];
 	char opening_mode[MODE_LENGTH];
@@ -40,7 +40,6 @@ SO_FILE *so_fopen(const char *pathname, const char *mode) {
 	memset(f->buffer, '\0', BUFF_SIZE);
 	strncpy(f->file_name, pathname, PATH_LENGTH);
 	strncpy(f->opening_mode, mode, MODE_LENGTH);
-	//f->bytes_read_last_time = 0;
 	f->error = 0;
 	return f;
 }
@@ -53,7 +52,7 @@ int so_fclose(SO_FILE *stream) {
 }
 
 int so_fileno(SO_FILE *stream) {
-	return 0;
+	return stream->fd;
 }
 
 int so_fflush(SO_FILE *stream) {
@@ -69,18 +68,39 @@ long so_ftell(SO_FILE *stream) {
 }
 
 size_t so_fread(void *ptr, size_t size, size_t nmemb, SO_FILE *stream) {
-	return 0;
+	int total_nr_of_bytes = (int)(size * nmemb);
+	int bytes_read = 0;
+
+	char *buf = calloc(total_nr_of_bytes, sizeof(char));
+
+	if (!buf) {
+		return 0;
+	}
+
+	while (bytes_read < total_nr_of_bytes) {
+		char ch = (char)so_fgetc(stream);
+
+		if (so_feof(stream) || so_ferror(stream)) {
+			break;
+		}
+
+		buf[bytes_read++] = ch;
+	}
+
+	memcpy(ptr, buf, bytes_read);
+	free(buf);
+	return (bytes_read / size);
 }
 
 size_t so_fwrite(const void *ptr, size_t size, size_t nmemb, SO_FILE *stream) {
 	return 0;
 }
 
+
 int so_fgetc(SO_FILE *stream) {
 	/* We check if the file is opened under read mode */
 	if (readRight(stream->opening_mode)) {
-		if (!stream->buffer_pointer || stream->buffer_pointer == BUFF_SIZE ||
-			stream->buffer_pointer == (int)strnlen(stream->buffer, BUFF_SIZE)) {
+		if (stream->buffer_pointer == (int)strnlen(stream->buffer, BUFF_SIZE)) {
 			memset(stream->buffer, '\0', BUFF_SIZE); /* Make sure the buffer is clean */
 			int rv = (int)read(stream->fd, stream->buffer, BUFF_SIZE);
 
@@ -94,16 +114,12 @@ int so_fgetc(SO_FILE *stream) {
 				return SO_EOF;
 			}
 
-			// stream->bytes_read_last_time = rv;
 			stream->buffer_pointer = 0; /* Make sure we read from the beginning of the buffer */
-			stream->file_pointer++;
-			return (int) (stream->buffer[stream->buffer_pointer++]);
 		}
+		stream->file_pointer++;
 
-		if (stream->buffer_pointer < (int)strnlen(stream->buffer, BUFF_SIZE)) {
-			stream->file_pointer++;
-			return (int) (stream->buffer[stream->buffer_pointer++]);
-		}
+		unsigned char c = stream->buffer[stream->buffer_pointer++];
+		return (int) c;
 	}
 
 	stream->error = 1; /* We couldn't read a character, so we have an error */
@@ -115,11 +131,11 @@ int so_fputc(int c, SO_FILE *stream) {
 }
 
 int so_feof(SO_FILE *stream) {
-	return 0;
+	return stream->eof_reached;
 }
 
 int so_ferror(SO_FILE *stream) {
-	return 0;
+	return stream->error;
 }
 
 SO_FILE *so_popen(const char *command, const char *type) {
